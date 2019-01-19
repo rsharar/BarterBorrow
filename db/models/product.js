b6826b61a71bce0e77a21b2a98d75e655ca23cbe
Rudy Sharar <rudy@activelylearn.com>
2019-01-19 12:39:19 -0800

    added productSchema

---
+++ db/models/product.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+mongoose.promise = Promise
+
+// Define productSchema
+// TODO: add validation for image
+const productSchema = new Schema({
+	owneruserId: { type: Number, unique: false, required: true },
+	description: { type: String, unique: false, required: true },
+    image: { type: String, unique: true, required: true },
+    category: { type: String, unique: false, required: true },
+    location: { type: String, unique: false, required: true },
+    status: { type: String, unique: false, required: true },
+	createdAt: { 
+		type: Date, required: true, default: Date.now }
+})
+
+// Create reference to User & export
+const Product = mongoose.model('Product', productSchema)
+module.exports = Product
