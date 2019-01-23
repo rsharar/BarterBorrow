1b36e9cdcf2c198344b16c29eb417812e8efd288
Loafus <eth.fish@gmail.com>
2019-01-22 18:57:03 -0800

    node change

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1934f4a3db9edc298451dfc18b7b9c36.js"
+  "/precache-manifest.62138263d446d735eeebc89a31cb983f.js"
 );
 
 workbox.clientsClaim();
