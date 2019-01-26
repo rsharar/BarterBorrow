6152961b8dc03f35e88f85cb5aad9e7adcf15a5e
Loafus <eth.fish@gmail.com>
2019-01-26 11:27:24 -0800

    minor change

--- routes/api/products.js
+++ routes/api/products.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const productController = require("../../controllers/productsController");
+const productController = require("../../controllers/productController");
 
 // Matches with "/api/products"
 router.route("/")
