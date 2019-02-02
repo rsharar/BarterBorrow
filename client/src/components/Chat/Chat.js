e19bdab7f09fedc6402d4ea160b7bc57c2b0483b
Loafus <eth.fish@gmail.com>
2019-02-02 11:52:42 -0800

    reading logged in username

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,8 +1,9 @@
 import React from "react";
 import io from "socket.io-client";
+import axios from 'axios'
 
-class Chat extends React.Component{
-    constructor(props){
+class Chat extends React.Component {
+    constructor(props) {
         super(props);
 
         this.state = {
@@ -13,14 +14,15 @@ class Chat extends React.Component{
 
         this.socket = io('localhost:80');
 
-        this.socket.on('RECEIVE_MESSAGE', function(data){
+        this.socket.on('RECEIVE_MESSAGE', function (data) {
             // console.log("FRONT END RECEIPT")
+            console.log(data)
             addMessage(data);
         });
 
         const addMessage = data => {
             // console.log(data);
-            this.setState({messages: [...this.state.messages, data]});
+            this.setState({ messages: [...this.state.messages, data] });
             console.log("Message: ");
             console.log(this.state.messages);
         };
@@ -31,11 +33,32 @@ class Chat extends React.Component{
                 author: this.state.username,
                 message: this.state.message
             })
-            this.setState({message: ''});
+            this.setState({ message: '' });
             // console.log("FRONT END SEND")
         }
     }
-    render(){
+
+    componentDidMount() {
+        axios.get('/auth/user').then(response => {
+            if (!!response.data.user) {
+                if (response.data.user.firstName) {
+                    // google firstname
+                    this.setState({
+                        username: response.data.user.firstName
+                    })
+                } else {
+                    // local username
+                    this.setState({
+                        username: response.data.user.local.username
+                    })
+                }
+            } else {
+                console.log("USER NOT LOGGED IN")
+            }
+        })
+    }
+
+    render() {
         return (
             <div className="container">
                 <div className="row">
@@ -43,7 +66,7 @@ class Chat extends React.Component{
                         <div className="card">
                             <div className="card-body">
                                 <div className="card-title">Global Chat</div>
-                                <hr/>
+                                <hr />
                                 <div className="messages">
                                     {this.state.messages.map(message => {
                                         return (
@@ -54,10 +77,10 @@ class Chat extends React.Component{
 
                             </div>
                             <div className="card-footer">
-                                <input type="text" placeholder="Username" value={this.state.username} onChange={ev => this.setState({username: ev.target.value})} className="form-control"/>
-                                <br/>
-                                <input type="text" placeholder="Message" className="form-control" value={this.state.message} onChange={ev => this.setState({message: ev.target.value})}/>
-                                <br/>
+                                <input type="text" placeholder="Username" value={this.state.username} className="form-control" />
+                                <br />
+                                <input type="text" placeholder="Message" className="form-control" value={this.state.message} onChange={ev => this.setState({ message: ev.target.value })} />
+                                <br />
                                 <button onClick={this.sendMessage} className="btn btn-primary form-control">Send</button>
                             </div>
                         </div>
