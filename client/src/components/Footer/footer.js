013d859961f41a616b88251793ad5baebcd78e8a
Albert Coder <eralbert9191@gmail.com>
2019-02-07 15:59:07 -0800

    Start with create-react-app

--- client/src/components/Footer/footer.js
+++ client/src/components/Footer/footer.js
@@ -8,11 +8,15 @@ const Footer = props => {
           <div className="container">
             <div className="row">
               <div className="col l6 s12">
-                <h5 className="white-text">Footer Content</h5>
-                <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+                <h5 className="white-text">Echo</h5>
+                <p className="grey-text text-lighten-4">Share and care about your community.</p>
+              </div>
+              <div className="col l6 s12">
+                <h5 className="white-text">Meet the team</h5>
+                <p className="grey-text text-lighten-4">Share and care about your community.</p>
               </div>
               <div className="col l4 offset-l2 s12">
-                <h5 className="white-text">Links</h5>
+                <h5 className="white-text">Talk to us!</h5>
                 <ul>
                   <li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li>
                   <li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
@@ -24,8 +28,10 @@ const Footer = props => {
           </div>
           <div className="footer-copyright">
             <div className="container">
-            © 2014 Copyright Text
-            <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
+            <strong>Echo</strong><a href="https://github.com/rsharar/BarterBorrow"> source code</a>. The source code is
+                licensed 
+                <a href="http://opensource.org/licenses/mit-license.php"> MIT</a>. The website content
+                is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
             </div>
           </div>
         </footer>
